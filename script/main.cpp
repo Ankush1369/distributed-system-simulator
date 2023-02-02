8295daef94f1995d49686a25b19bee7996a4b220
Ankush1369 <ankush2001garg@gmail.com>
2023-02-02 23:31:39 +0530

    Adding boilerplate file

---
+++ script/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <omp.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class System{
+    public:
+        System(){
+            
+        }
+    private:
+
+};
+
+void transfer()
+
+int main(int argc, char* argv[]){
+
+}
