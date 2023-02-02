36889e053794fa37fb85ff5baae1301885687ea6
Ankush1369 <ankush2001garg@gmail.com>
2023-02-03 02:32:17 +0530

    adding .gitignore file and updating Initializer function

--- script/main.cpp
+++ script/main.cpp
@@ -2,6 +2,8 @@
 #include <omp.h>
 #include <vector>
 #include <algorithm>
+#include <assert.h>
+#include <fstream>
 
 using namespace std;
 
@@ -15,11 +17,16 @@ class System{
 
 };
 
+vector<System> NODES;
+vector<vector<int> > FROM_EDGES;
+vector<vector<int> > TO_EDGES;
+int N;
+
 void getData(){
     
 }
 
-void transfer(){
+void transfer(int from, int to){
 
 }
 
@@ -31,7 +38,34 @@ void clockCycleEnd(){
 
 }
 
+void Initialize(string inputFile){
+    ifstream inputGraph( (string) inputFile);
+    inputGraph >> N;
+    assert(N > 1);
+    
+    FROM_EDGES.resize(N+1);
+    TO_EDGES.resize(N+1);
+    NODES.resize(N+1);
+
+    int edgeCount;
+    inputGraph >> edgeCount;
+
+    while(edgeCount--){
+        int fromNode, toNode;
+        inputGraph >> fromNode >> toNode;
+        
+        assert(fromNode < N+1);
+        assert(fromNode > 0);
+
+        assert(toNode < N+1);
+        assert(toNode > 0);
+    }
+}
+
+
+
 
 int main(int argc, char* argv[]){
-
+    assert(argc == 2);
+    
 }
